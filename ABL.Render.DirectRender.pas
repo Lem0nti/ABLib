6b1589a401105f1470826190a80c3bd10890d37c
Lem0nti <admin@wrappers.ru>
2022-11-17 21:46:18 +0300

    Рендер - ссылка на последний кадр

--- ABL.Render.DirectRender.pas
+++ ABL.Render.DirectRender.pas
@@ -14,21 +14,24 @@ type
   TDirectRender=class(TDirectThread)
   private
     FDrawer: TDrawer;
-    FWidth, FHeight: integer;
-    FLastPicture: PDecodedFrame;
+    //FWidth, FHeight: integer;
+    FLastPicture,OldData: PDecodedFrame;
     SkipThru: TDateTime;
     FHandle: THandle;
+    FPaused: boolean;
     procedure UpdateSizes;
     function GetHandle: THandle;
-    function GetHeight: integer;
-    function GetWidth: integer;
+//    function GetHeight: integer;
+//    function GetWidth: integer;
     procedure SetHandle(const Value: THandle);
-    procedure SetHeight(const Value: integer);
-    procedure SetWidth(const Value: integer);
+//    procedure SetHeight(const Value: integer);
+//    procedure SetWidth(const Value: integer);
     function GetCameraName: string;
     procedure SetCameraName(const Value: string);
     function GetOnDraw: TDrawNotify;
     procedure SetOnDraw(const Value: TDrawNotify);
+    function GetPaused: boolean;
+    procedure SetPaused(const Value: boolean);
   protected
     {$IFDEF FPC}
     procedure ClearData(AData: Pointer); override;
@@ -37,15 +40,17 @@ type
   public
     constructor Create(AName: string = ''); override;
     destructor Destroy; override;
-    procedure SetSize(AWidth, AHeight: integer);
+    function LastPicture(Original: boolean = false): PDecodedFrame;
+//    procedure SetSize(AWidth, AHeight: integer);
     procedure SkipSecond;
     procedure UpdateScreen;
     property CameraName: string read GetCameraName write SetCameraName;
     property Drawer: TDrawer read FDrawer;
     property Handle: THandle read GetHandle write SetHandle;
-    property Height: integer read GetHeight write SetHeight;
+    //property Height: integer read GetHeight write SetHeight;
     property OnDraw: TDrawNotify read GetOnDraw write SetOnDraw;
-    property Width: integer read GetWidth write SetWidth;
+    property Paused: boolean read GetPaused write SetPaused;
+    //property Width: integer read GetWidth write SetWidth;
   end;
 
 implementation
@@ -57,14 +62,17 @@ begin
   inherited Create(nil,nil,AName);
   FInputQueue:=TDecodedItem.Create(ClassName+'_'+AName+'_Input_'+IntToStr(FID));
   FDrawer:=TDrawer.Create(0,ClassName+'_'+AName+'_Drawer_'+IntToStr(FID));
-  FWidth:=1920;
-  FHeight:=1080;
+//  FWidth:=1920;
+//  FHeight:=1080;
   SkipThru:=0;
   FLastPicture:=nil;
   Active:=true;
   new(FLastPicture);
   FLastPicture.Time:=0;
   GetMem(FLastPicture.Data,3000*2000*3);
+  FPaused:=false;
+  new(OldData);
+  GetMem(OldData.Data,3000*2000*3);
 end;
 
 destructor TDirectRender.Destroy;
@@ -88,13 +96,20 @@ begin
   try
     if (SkipThru<now) and assigned(FDrawer) then
     begin
-      DrawResult:=FDrawer.Draw(rData);
-      Move(rData.Data^,FLastPicture.Data^,rData.Width*rData.Height*3);
-      FLastPicture.Width:=rData.Width;
-      FLastPicture.Height:=rData.Height;
-      FLastPicture.Time:=rData.Time;
-      if DrawResult<0 then
-        SkipSecond;
+      if not FPaused then
+      begin
+        OldData.Width:=rData.Width;
+        OldData.Height:=rData.Height;
+        OldData.Time:=rData.Time;
+        Move(rData.Data^,OldData.Data^,rData.Width*rData.Height*3);
+        DrawResult:=FDrawer.Draw(rData);
+        Move(rData.Data^,FLastPicture.Data^,rData.Width*rData.Height*3);
+        FLastPicture.Width:=rData.Width;
+        FLastPicture.Height:=rData.Height;
+        FLastPicture.Time:=rData.Time;
+        if DrawResult<0 then
+          SkipSecond;
+      end;
     end;
   finally
     try
@@ -124,28 +139,66 @@ begin
   end;
 end;
 
-function TDirectRender.GetHeight: integer;
-begin
-  FLock.Enter;
-  try
-    result:=FHeight;
-  finally
-    FLock.Leave;
-  end;
-end;
+//function TDirectRender.GetHeight: integer;
+//begin
+//  FLock.Enter;
+//  try
+//    result:=FHeight;
+//  finally
+//    FLock.Leave;
+//  end;
+//end;
 
 function TDirectRender.GetOnDraw: TDrawNotify;
 begin
   result:=FDrawer.OnDraw;
 end;
 
-function TDirectRender.GetWidth: integer;
+function TDirectRender.GetPaused: boolean;
 begin
   FLock.Enter;
   try
-    result:=FWidth;
+    result:=FPaused;
   finally
     FLock.Leave;
+  end;
+end;
+
+//function TDirectRender.GetWidth: integer;
+//begin
+//  FLock.Enter;
+//  try
+//    result:=FWidth;
+//  finally
+//    FLock.Leave;
+//  end;
+//end;
+
+function TDirectRender.LastPicture(Original: boolean = false): PDecodedFrame;
+var
+  DecodedFrame: PDecodedFrame;
+begin
+  result:=nil;
+  if Original then
+    DecodedFrame:=OldData
+  else
+    DecodedFrame:=FLastPicture;
+  if assigned(DecodedFrame) then
+  begin
+    New(result);
+    FLock.Enter;
+    try
+      result.Time:=DecodedFrame.Time;
+      result.Width:=DecodedFrame.Width;
+      result.Height:=DecodedFrame.Height;
+      result.Left:=DecodedFrame.Left;
+      result.Top:=DecodedFrame.Top;
+      result.ImageType:=DecodedFrame.ImageType;
+      GetMem(result.Data,result.Width*result.Height*3);
+      Move(DecodedFrame.Data^,result.Data^,result.Width*result.Height*3);
+    finally
+      FLock.Leave;
+    end;
   end;
 end;
 
@@ -165,53 +218,63 @@ begin
   end;
 end;
 
-procedure TDirectRender.SetHeight(const Value: integer);
-begin
-  FLock.Enter;
-  try
-    if FHeight<>Value then
-    begin
-      FHeight:=Value;
-      UpdateSizes;
-    end;
-  finally
-    FLock.Leave;
-  end;
-end;
+//procedure TDirectRender.SetHeight(const Value: integer);
+//begin
+//  FLock.Enter;
+//  try
+//    if FHeight<>Value then
+//    begin
+//      FHeight:=Value;
+//      UpdateSizes;
+//    end;
+//  finally
+//    FLock.Leave;
+//  end;
+//end;
 
 procedure TDirectRender.SetOnDraw(const Value: TDrawNotify);
 begin
   FDrawer.OnDraw:=Value;
 end;
 
-procedure TDirectRender.SetSize(AWidth, AHeight: integer);
+procedure TDirectRender.SetPaused(const Value: boolean);
 begin
   FLock.Enter;
   try
-    if (FWidth<>AWidth)or(FHeight<>AHeight) then
-    begin
-      FHeight:=AHeight;
-      FWidth:=AWidth;
-      UpdateSizes;
-    end;
+    FPaused:=Value;
   finally
     FLock.Leave;
   end;
 end;
 
-procedure TDirectRender.SetWidth(const Value: integer);
-begin
-  FLock.Enter;
-  try
-    if FWidth<>Value then
-    begin
-      FWidth:=Value;
-      UpdateSizes;
-    end;
-  finally
-    FLock.Leave;
-  end;
-end;
+//procedure TDirectRender.SetSize(AWidth, AHeight: integer);
+//begin
+//  FLock.Enter;
+//  try
+//    if (FWidth<>AWidth)or(FHeight<>AHeight) then
+//    begin
+//      FHeight:=AHeight;
+//      FWidth:=AWidth;
+//      UpdateSizes;
+//    end;
+//  finally
+//    FLock.Leave;
+//  end;
+//end;
+
+//procedure TDirectRender.SetWidth(const Value: integer);
+//begin
+//  FLock.Enter;
+//  try
+//    if FWidth<>Value then
+//    begin
+//      FWidth:=Value;
+//      UpdateSizes;
+//    end;
+//  finally
+//    FLock.Leave;
+//  end;
+//end;
 
 {$IFDEF FPC}
 procedure TDirectRender.ClearData(AData: Pointer);
