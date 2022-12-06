ae05c772398c1ac7a8bac84cca15f7d7bb8d9973
Lem0nti <admin@wrappers.ru>
2022-12-06 22:02:36 +0300

    TFindDark - работа с чб картинками

--- ABL.IA.FindDark.pas
+++ ABL.IA.FindDark.pas
@@ -3,7 +3,7 @@ unit ABL.IA.FindDark;
 interface
 
 uses
-  ABL.Core.DirectThread, ABL.Core.BaseQueue, ABL.VS.VSTypes, ABL.IA.IATypes;
+  ABL.Core.DirectThread, ABL.Core.BaseQueue, ABL.VS.VSTypes, ABL.IA.IATypes, SysUtils;
 
 type
   TFindDark=class(TDirectThread)
@@ -40,39 +40,66 @@ var
   x,y,CurrentPixel,CurrentByte,CurrentBit: integer;
   DecodedFrame: PImageDataHeader;
   RGBLine,RGBLineTop,RGBLineBottom: PRGBArray;
+  ByteLine,ByteLineTop,ByteLineBottom: PByteArray;
   ResultPointer: PByte;
   tmpDataSize: integer;
   CurGreen: byte;
 begin
   DecodedFrame:=AInputData;
-  if DecodedFrame.ImageType=itBGR then
+  if DecodedFrame.ImageType in [itBGR,itGray] then
   begin
     tmpDataSize:=((DecodedFrame.Width*DecodedFrame.Height) div 8)+1;
     FillChar(tmpBuffer^,tmpDataSize,255);
-    for y:=1 to DecodedFrame.Height-2 do
-    begin
-      RGBLine:=PRGBArray(NativeUInt(DecodedFrame.Data)+y*DecodedFrame.Width*3);
-      RGBLineTop:=PRGBArray(NativeUInt(DecodedFrame.Data)+(y-1)*DecodedFrame.Width*3);
-      RGBLineBottom:=PRGBArray(NativeUInt(DecodedFrame.Data)+(y+1)*DecodedFrame.Width*3);
-      for x:=1 to DecodedFrame.Width-2 do
+    if DecodedFrame.ImageType=itBGR then
+      for y:=1 to DecodedFrame.Height-2 do
       begin
-        CurrentPixel:=y*DecodedFrame.Width+x;
-        CurrentByte:=CurrentPixel div 8;
-        CurrentBit:=CurrentPixel mod 8;
-        CurGreen:=Round(RGBLine[x].rgbtGreen*GreenDec);
-        if RGBLineTop[x-1].rgbtGreen>CurGreen then
-          if RGBLineTop[x].rgbtGreen>CurGreen then
-            if RGBLineTop[x+1].rgbtGreen>CurGreen then
-              if RGBLine[x-1].rgbtGreen>CurGreen then
-                if RGBLine[x+1].rgbtGreen>CurGreen then
-                  if RGBLineBottom[x-1].rgbtGreen>CurGreen then
-                    if RGBLineBottom[x].rgbtGreen>CurGreen then
-                      if RGBLineBottom[x+1].rgbtGreen>CurGreen then
-                        Continue;
-        ResultPointer:=PByte(NativeUInt(tmpBuffer)+CurrentByte);
-        ResultPointer^:=(ResultPointer^ and not (1 shl CurrentBit));
+        RGBLine:=PRGBArray(NativeUInt(DecodedFrame.Data)+y*DecodedFrame.Width*3);
+        RGBLineTop:=PRGBArray(NativeUInt(DecodedFrame.Data)+(y-1)*DecodedFrame.Width*3);
+        RGBLineBottom:=PRGBArray(NativeUInt(DecodedFrame.Data)+(y+1)*DecodedFrame.Width*3);
+        for x:=1 to DecodedFrame.Width-2 do
+        begin
+          CurrentPixel:=y*DecodedFrame.Width+x;
+          CurrentByte:=CurrentPixel div 8;
+          CurrentBit:=CurrentPixel mod 8;
+          CurGreen:=Round(RGBLine[x].rgbtGreen*GreenDec);
+          if RGBLineTop[x-1].rgbtGreen>CurGreen then
+            if RGBLineTop[x].rgbtGreen>CurGreen then
+              if RGBLineTop[x+1].rgbtGreen>CurGreen then
+                if RGBLine[x-1].rgbtGreen>CurGreen then
+                  if RGBLine[x+1].rgbtGreen>CurGreen then
+                    if RGBLineBottom[x-1].rgbtGreen>CurGreen then
+                      if RGBLineBottom[x].rgbtGreen>CurGreen then
+                        if RGBLineBottom[x+1].rgbtGreen>CurGreen then
+                          Continue;
+          ResultPointer:=PByte(NativeUInt(tmpBuffer)+CurrentByte);
+          ResultPointer^:=(ResultPointer^ and not (1 shl CurrentBit));
+        end;
+      end
+    else
+      for y:=1 to DecodedFrame.Height-2 do
+      begin
+        ByteLine:=PByteArray(NativeUInt(DecodedFrame.Data)+y*DecodedFrame.Width);
+        ByteLineTop:=PByteArray(NativeUInt(DecodedFrame.Data)+(y-1)*DecodedFrame.Width);
+        ByteLineBottom:=PByteArray(NativeUInt(DecodedFrame.Data)+(y+1)*DecodedFrame.Width);
+        for x:=1 to DecodedFrame.Width-2 do
+        begin
+          CurrentPixel:=y*DecodedFrame.Width+x;
+          CurrentByte:=CurrentPixel div 8;
+          CurrentBit:=CurrentPixel mod 8;
+          CurGreen:=Round(ByteLine[x]*GreenDec);
+          if ByteLineTop[x-1]>CurGreen then
+            if ByteLineTop[x]>CurGreen then
+              if ByteLineTop[x+1]>CurGreen then
+                if ByteLine[x-1]>CurGreen then
+                  if ByteLine[x+1]>CurGreen then
+                    if ByteLineBottom[x-1]>CurGreen then
+                      if ByteLineBottom[x]>CurGreen then
+                        if ByteLineBottom[x+1]>CurGreen then
+                          Continue;
+          ResultPointer:=PByte(NativeUInt(tmpBuffer)+CurrentByte);
+          ResultPointer^:=(ResultPointer^ and not (1 shl CurrentBit));
+        end;
       end;
-    end;
     DecodedFrame.ImageType:=itBit;
     if assigned(FOutputQueue) then
     begin
